e7824c059532f9dcbc6a32d43f1e4bae98e886b8
quickconnectzhang <139637128+quickconnectzhang@users.noreply.github.com>
2024-01-25 09:55:49 +0800

    Create quiz_10.js

---
+++ quiz_10.js
@@ -0,0 +1,42 @@
+const questionsAndAnswers = [
+    {
+        question: "Which Ethereum Layer-2 scaling solution processes transactions off-chain assuming them to be correct unless proven otherwise requiring a dispute resolution window?",
+        answerText: "Optimistic Rollups"
+    },
+    {
+        question: "Which (Ethereum) Layer-2 scaling solution utilizes zero-knowledge proofs to cryptographically ensure every transaction's correctness without requiring fraud verification windows or challenge periods?",
+        answerText: "zk-Rollups"
+    },
+    {
+        question: "What is the common objective that blockchain bridges, atomic swaps, and synthetics aim to achieve in the Web3 ecosystem?",
+        answerText: "Facilitate seamless interactions or value transfer across different blockchains"
+    },
+    {
+        question: "_____ platforms aggregate and interpret vast amounts of on-chain data to produce useful metrics and insights about network health, economic activity, market sentiment, and more.",
+        answerText: "On-chain Analytics"
+    },
+    {
+        question: "Which of the following best describes the primary function of the MetaMask web browser extension?",
+        answerText: "A wallet interface for interacting with Ethereum-based dapps"
+    },
+    {
+        question: "What role does a blockchain explorer play in the blockchain ecosystem?",
+        answerText: "It enables users to view detailed information about blocks, transactions, and addresses"
+    },
+    {
+        question: "Why are oracles considered an important blockchain interface?",
+        answerText: "They allow blockchains and its smart contracts to interact with external data sources"
+    },
+    {
+        question: "Which statement best describes the differences among lightweight wallets, full-node wallets, and third-party wallet applications in the context of blockchain?",
+        answerText: "Lightweight wallets don't store the entire blockchain but allow transactions, full-node wallets store and verify the entire blockchain, and third-party wallets are applications developed by external entities to manage keys and transact."
+    },
+    {
+        question: "_____  are blockchain systems that run parallel to a “mainchain\" or L1 blockchain providing a mechanism to offload transactions from the mainchain, while performing their own consensus mechanism to reduce congestion and allow for faster processing times and interoperability.",
+        answerText: "Sidechains"
+    },
+    {
+        question: "What is the use case of Zero-Knowledge Proofs (zk-proofs) technology in blockchain?",
+        answerText: "All of the above"
+    }
+];
