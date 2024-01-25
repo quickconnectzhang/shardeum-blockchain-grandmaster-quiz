14ac479ffda63ce745cd54c9c4c330293ee1fea8
quickconnectzhang <139637128+quickconnectzhang@users.noreply.github.com>
2024-01-25 09:36:18 +0800

    Create quiz_3.js

---
+++ quiz_3.js
@@ -0,0 +1,42 @@
+const questionsAndAnswers = [
+    {
+        question: "What is the primary reason for keeping a safe backup of your passphrase (or seed phrase/backup codes) when using blockchain wallets, even though other security measures mentioned here may also apply?",
+        answerText: "To recover your account if you lose your private keys"
+    },
+    {
+        question: "How can Proof-of-Stake (PoS) networks, such as Ethereum, achieve a security level comparable to that of Proof-of-Work (PoW) networks like Bitcoin?",
+        answerText: "Through a system where validators stake assets as collateral"
+    },
+    {
+        question: "Why do experts often emphasize addressing security issues at Layer 1 of a blockchain rather than relying solely on Layer 2 blockchains and other scaling solutions?",
+        answerText: "Layer 1s is the foundational layer known to provide a very high level of security and decentralization, benefitting the entire network and all dapps built upon it"
+    },
+    {
+        question: "As modern blockchain networks strive to reduce transaction fees, which attack type lets attackers cheaply flood the network with low cost requests to slow/disrupt it?",
+        answerText: "DDoS attack"
+    },
+    {
+        question: "Consider a malicious attacker creating multiple pseudonymous identities by taking control over a node or group of nodes. These nodes are then used to spread misinformation, disrupt consensus, or attempt to influence the network's decisions. What is the nefarious act known as in the context of blockchain?",
+        answerText: "Sybil attack"
+    },
+    {
+        question: "Asides from hashing, which other key cryptographic method does blockchain technology employ to ensure that data doesn’t change during the broadcast of validated transactions between nodes and throughout all blockchain transactions for that matter?",
+        answerText: "Digital signatures"
+    },
+    {
+        question: "Which term is commonly associated with the practice of prioritizing certain transactions on a blockchain by reordering them for potential financial gains, especially in the context of DeFi platforms?",
+        answerText: "MEV (Miner Extractable Value)"
+    },
+    {
+        question: "Safe deposit boxes (or bank lockers) are secured by a key that only the owner (custodians like banks) have access to. Similarly, what protects digital assets owned by users themselves on a blockchain?",
+        answerText: "A pair of public and private keys"
+    },
+    {
+        question: "Beyond the consensus mechanism, which of the below is the most ideal technique sharded blockchains can employ to maintain the high security levels that public blockchains are known for?",
+        answerText: "Auto-rotation of nodes"
+    },
+    {
+        question: "Which is the most battle-tested, highly secure hashing algorithm used by various blockchain networks to handle transactions and record data?",
+        answerText: "SHA-2 and SHA-3"
+    }
+];
